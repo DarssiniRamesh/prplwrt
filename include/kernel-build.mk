c900fde7d4a67976dcd18c6db0d0133e2be72455
Marvin Lu <marvin.lu@wnc.com.tw>
2023-12-22 17:52:11 +0000

    build: introduce KERNEL_GIT_BRANCH feature
    Using large external Git kernel repositories can be challenging. To
    facilitate this, a new KERNEL_GIT_BRANCH feature is added. This feature
    allows specifying a Git branch during the cloning process, aiding in
    efficiently reaching the desired Git reference in subsequent checkout
    step.
    
    This enhancement addresses situation with the DownloadMethod/rawgit
    functionality, which performs 'git clone' and 'git checkout'. In cases
    of shallow clones, the desired Git reference may not be accessible,
    making it necessary to specify the branch separately.
    
    Signed-off-by: Marvin Lu <marvin.lu@wnc.com.tw>
    Signed-off-by: Petr Štetiar <ynezz@true.cz> [refactor]
    (cherry picked from commit 4b55bca74e4e1d0c990177acd220d164ae8ae15c)

--- include/kernel-build.mk
+++ include/kernel-build.mk
@@ -52,6 +52,10 @@ ifneq ($(strip $(CONFIG_KERNEL_GIT_DEPTH)),"")
   KERNEL_GIT_OPTS+=--depth $(CONFIG_KERNEL_GIT_DEPTH)
 endif
 
+ifneq ($(strip $(CONFIG_KERNEL_GIT_BRANCH)),"")
+  KERNEL_GIT_OPTS+=--branch $(CONFIG_KERNEL_GIT_BRANCH)
+endif
+
 define Download/git-kernel
   URL:=$(call qstrip,$(CONFIG_KERNEL_GIT_CLONE_URI))
   PROTO:=git
