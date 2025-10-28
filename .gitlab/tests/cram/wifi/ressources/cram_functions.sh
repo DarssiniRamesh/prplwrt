074d17da9cb766d9bee13c3e6caac5e43a867a73
Houssem Dafdouf <houssem.dafdouf_ext@softathome.com>
2025-10-28 23:27:24 +0100

    ci: cram: move all common helpers into a single script
    The wifi helpers definitions were duplicated across multiple cram
    scripts.
    
    Let's consolidated them into a single script that can be sourced from
    the CRAM scripts each time it's needed.
    
    Closes: PPW-230
    
    Signed-off-by: Houssem Dafdouf <houssem.dafdouf_ext@softathome.com>

---
+++ .gitlab/tests/cram/wifi/ressources/cram_functions.sh
@@ -0,0 +1,53 @@
+#
+# Common WiFi helpers:
+#
+
+# Enable AccessPoints
+# In : AccessPoint object index
+# Out : "enabled" if success, empty otherwise
+enable_ap() {
+  R "ba-cli -j -l WiFi.AccessPoint.${1}.Enable=1 | grep -q Enable && echo 'WiFi.AccessPoint.${1} enabled'"
+}
+
+# Disable AccessPoints
+# In : AccessPoint object index
+# Out : "disabled" if success, empty otherwise
+disable_ap() {
+  R "ba-cli -j -l WiFi.AccessPoint.${1}.Enable=0 | grep -q Enable && echo 'WiFi.AccessPoint.${1} disabled'"
+}
+
+# Wait until SSID status is Up/Down
+# In : AccessPoint object index
+# In : Expected status Up/Down
+# Out: "SSID Reference is {Up/Down}"
+check_ap_ref_ssid() {
+  R "
+    i=10
+    while [ \$i -gt 1 ]; do
+      ba-cli -j -l WiFi.AccessPoint.${1}.SSIDReference+.Status? |
+        grep WiFi.SSID. |
+        grep -q \"${2}\" &&
+        echo 'WiFi.AccessPoint.${1} SSID Reference is ${2}' && break
+      i=\$(( i - 1 ))
+      sleep 2
+    done
+  "
+}
+
+# Print SSIDReference status
+# In : AccessPoint object index
+# Out : Enable / Disable / Dormant ...
+get_ssid_ref() {
+  msg=$(R "ba-cli -j -l WiFi.AccessPoint.${1}.SSIDReference+.Status?")
+  echo "$msg" | sed '/^$/d'
+}
+
+# Print SSIDs status
+get_ssid_status() {
+  R "ba-cli -j -l WiFi.SSID.?0 | jsonfilter -e @[0]'[@.Alias != \"ep2g0\" && @.Alias != \"ep5g0\" && @.Alias != \"ep6g0\"].Status'" | LC_ALL=C sort
+}
+
+# Print SSIDs values
+get_ssid_ssid() {
+  R "ba-cli -j -l WiFi.SSID.?0 | jsonfilter -e @[0]'[@.Alias != \"ep2g0\" && @.Alias != \"ep5g0\" && @.Alias != \"ep6g0\"].SSID'" | LC_ALL=C sort
+}
